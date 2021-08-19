1f7bcda835a4797a7b062f7a0a62db0e4cc092c8
Marcin Czekaj <czekajmarcin@op.pl>
2021-08-19 22:59:13 +0200

    added info about this class

--- src/main/java/pl/czekaj/springsocial/aspect/RestControllerAspect.java
+++ src/main/java/pl/czekaj/springsocial/aspect/RestControllerAspect.java
@@ -9,6 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+
+/*
+	The task of this class is only to show
+	my knowledge about AOP expect advices in exception packages
+ */
+
 @Aspect
 //@Component
 public class RestControllerAspect {
